78f8d335ec65ec5ee4c61f53e03516e76abc5145
KateC08 <coming.katelyn08@gmail.com>
2024-09-08 21:08:07 +0800

    Added functions to determine whether a number is a Prime or Not

--- kate.js
+++ kate.js
@@ -1,3 +1,24 @@
 //Data types
 let k_name = "level";
 let num = 5;
+
+function Prime_or_Not(num) {
+    //Conditional statements
+    if(num === 2) {
+        return num + " is a Prime Number";
+    } else if (num === 1) {
+        return num + " is not a Prime Number";
+    } else if(num % 2 === 0) {
+        return num + " is not a Prime Number";
+    }else {
+        return num + " is a Prime Number";
+    }
+
+} 
+//Loops
+for(i = 0; i <= num; i++) {
+    console.log(Prime_or_Not(i));
+}
+console.log("~~~~~~~~~~~~~~~~~");
+
+
